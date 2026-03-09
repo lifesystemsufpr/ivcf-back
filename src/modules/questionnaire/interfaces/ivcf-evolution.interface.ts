1ab907356a79885b254e8248e9d1a3af9fb7ef59
garzuze <lucasgarzuze@gmail.com>
2026-03-09 11:09:33 -0300

    feat: create history, domain and details related interfaces

--- src/modules/questionnaire/interfaces/ivcf-evolution.interface.ts
+++ src/modules/questionnaire/interfaces/ivcf-evolution.interface.ts
@@ -29,3 +29,50 @@ export interface ParticipantEvolutionResponse {
   participantName: string;
   assessments: IvcfAssessment[];
 }
+
+export interface ParticipantSummaryResponse {
+  participantId: string;
+  participantName: string;
+  totalAssessments: number;
+  lastAssessment: {
+    id: string;
+    date: string;
+    totalScore: number;
+    riskLevel: string;
+    domains: IvcfDomainScores;
+  } | null;
+}
+
+export interface ScoreHistoryItem {
+  id: string;
+  date: string;
+  totalScore: number;
+  riskLevel: string;
+}
+
+export interface ScoreHistoryResponse {
+  participantId: string;
+  participantName: string;
+  scores: ScoreHistoryItem[];
+}
+
+export interface DomainHistoryItem {
+  id: string;
+  date: string;
+  domains: IvcfDomainScores;
+}
+
+export interface DomainHistoryResponse {
+  participantId: string;
+  participantName: string;
+  history: DomainHistoryItem[];
+}
+
+export interface AssessmentDetailResponse {
+  id: string;
+  date: string;
+  totalScore: number;
+  riskLevel: string;
+  domains: IvcfDomainScores;
+  rawResponses: Record<string, string>;
+}
